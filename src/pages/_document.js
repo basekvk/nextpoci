4af2c3be8fc3cc3565da828f0fa568e37e789dbe
David García <basek32@gmail.com>
2023-12-15 11:12:38 +0100

    15 de diciembre 23 v2

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,37 +1,38 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import LocalBusinessSchema from '../components/localbusinessschema';
 
+const MyDocument = () => {
+    return (
+        <Html lang="es">
+            <Head>
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin="true"
+                />
+                <link
+                    rel="preload"
+                    href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&family=Yantramanav:wght@300;400;500;700;900&display=swap"
+                    as="style"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&family=Yantramanav:wght@300;400;500;700;900&display=swap"
+                    rel="stylesheet"
+                />
 
+                {/* Considera eliminar o cargar de forma diferida Font Awesome si no es esencial */}
+                <link
+                    rel="stylesheet"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+                />
 
-
-class MyDocument extends Document {
-    render() {
-        return (
-            <Html lang="es">
-                <Head>
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&family=Yantramanav:wght@300;400;500;700;900&display=swap"
-                        rel="stylesheet"
-                    />
-
-                    <link
-                        rel="stylesheet"
-                        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-                    />
-                    <LocalBusinessSchema />
-                    
-                </Head>
-                <body>
-                    <Main />
-                  
-                    <NextScript />
-                   
-                    
-                    
-                </body>
-            </Html>
-        );
-    }
-}
+                {/* Tu componente LocalBusinessSchema aquí */}
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+};
 
 export default MyDocument;
