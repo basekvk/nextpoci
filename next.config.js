85044e86eaec828da07f89f1d5f13008eec2ca25
David García <basek32@gmail.com>
2023-12-18 15:43:25 +0100

    18 de diciembre mañana v11

--- next.config.js
+++ next.config.js
@@ -4,12 +4,24 @@
 const nextConfig = {
     reactStrictMode: true,
     optimizeFonts: true,
-    optimization: true
+    optimization: true,
+    images: {
+        domains: ['desatascos-madrid.com'],
+        formats: ['image/avif', 'image/webp'],
+    },
+    /*async redirects() {
+        return [
+          {
+            source: '', // Ruta original que quieres redirigir
+            destination: '', // Nueva ruta a la que se redirigirá
+            permanent: true, // Esto hace que sea una redirección 301
+        },// Tus redirecciones aquí
+        ];
+    },*/
 };
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
-  })
-  module.exports = withBundleAnalyzer({})
-  
+});
+module.exports = withBundleAnalyzer({});
+
 module.exports = nextConfig;
- 
