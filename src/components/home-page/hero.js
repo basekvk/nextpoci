4a7611bbd5ea2fa84019a887e1bf76a16542bffa
David García <basek32@gmail.com>
2024-01-06 18:37:57 +0100

    6 de enero 24  v2

--- src/components/home-page/hero.js
+++ src/components/home-page/hero.js
@@ -10,21 +10,27 @@ function Hero({ heroItems }) {
     // Por ejemplo, .hero-container { position: relative; z-index: 1; }
 
     return (
-        <div className={classes.heroBackground} >
+        <div className={classes.heroBackground}>
             {/* La imagen de fondo */}
             <Image
                 src="/images/hero/1.webp"
-                alt="Descripción de la imagen"
+                alt="Poceria Pociten desatascos en Madrid Camion Cuba"
                 layout="fill"
                 objectFit="cover"
                 quality={75}
                 priority
             />
             {/* El contenedor y el contenido */}
-            <div className={classes.heroContainer} style={{ position: 'absolute', top: 100, left: 30, width: '100%', height: '100%', zIndex: 1 }}>
-                <div className={classes.container}> {/* Asegúrate de que esta clase esté definida para contener y centrar tu contenido */}
-                    <div className={classes.content}> {/* Esta clase debería posicionar tu contenido sobre la imagen */}
-                        <span className={`subtitle-animation ${classes.subtitle}`}>
+            <div className={classes.heroContainer}>
+                <div className={classes.container}>
+                    {' '}
+                    {/* Asegúrate de que esta clase esté definida para contener y centrar tu contenido */}
+                    <div className={classes.content}>
+                        {' '}
+                        {/* Esta clase debería posicionar tu contenido sobre la imagen */}
+                        <span
+                            className={`subtitle-animation ${classes.subtitle}`}
+                        >
                             {heroItem?.subtitle}
                         </span>
                         <div
@@ -38,16 +44,19 @@ function Hero({ heroItems }) {
                             dangerouslySetInnerHTML={{
                                 __html: heroItem.desc,
                             }}
-                            style={{ maxWidth: 350, justifyContent: 'center', alignItems: 'center' }}
                         />
                         <div className={`btn-animation ${classes.btn_wrap}`}>
                             <Link href="/contacto">
-                                <a className={`me-20 ${classes.btn} ${classes.btn_secondary} ${classes.btn_hover__white}`}>
+                                <a
+                                    className={`me-20 ${classes.btn} ${classes.btn_secondary} ${classes.btn_hover__white}`}
+                                >
                                     {heroItem?.btnSecondaryText}
                                 </a>
                             </Link>
                             <Link href="tel:+34647376782">
-                                <a className={`${classes.btn} ${classes.btn_primary} ${classes.btn_hover__white}`}>
+                                <a
+                                    className={`${classes.btn} ${classes.btn_primary} ${classes.btn_hover__white}`}
+                                >
                                     {heroItem?.btnPrimaryText}
                                 </a>
                             </Link>
