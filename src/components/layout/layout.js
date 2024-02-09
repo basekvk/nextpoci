fef449b468f4208ef0ae4e4831abca8a2035256f
David García <basek32@gmail.com>
2024-02-09 20:28:07 +0100

    Vercel Analytics

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -2,39 +2,43 @@ import PropTypes from 'prop-types';
 import Header from './header';
 import 'react-whatsapp-widget/dist/index.css';
 import dynamic from 'next/dynamic';
-
+import { Analytics } from '@vercel/analytics/react';
 
 // Importar WhatsAppWidget y PhoneWidget dinámicamente sin SSR
-const WhatsAppWidget = dynamic(() => import('react-whatsapp-widget').then((mod) => mod.WhatsAppWidget),
-{ ssr: false }
+const WhatsAppWidget = dynamic(
+    () => import('react-whatsapp-widget').then((mod) => mod.WhatsAppWidget),
+    { ssr: false }
 );
-  
-  const PhoneWidget = dynamic(() => import('../callwidget'), {
+
+const PhoneWidget = dynamic(() => import('../callwidget'), {
     ssr: false,
-  });
-  
-  function Layout({ children }) {
-      return (
-          <>
-              <Header />
-              
-              <main>{children}</main>
-              
-              {/* Estos componentes solo se montarán en el cliente */}
-              <WhatsAppWidget
-                  phoneNumber="+34647376782"
-                  message="Hola! 👋🏼 ¿En qué podemos ayudarte?"
-                  sendButton="Enviar"
-                  companyName="Desatascos Pociten"
-                  textReplyTime="En breve te responderemos. Muchas gracias"
-              />
-              <PhoneWidget />
-          </>
-      );
-  }
-  
-  Layout.propTypes = {
-      children: PropTypes.node.isRequired,
-  };
-  
-  export default Layout;
+});
+
+function Layout({ children }) {
+    return (
+        <>
+            <Header />
+
+            <main>
+                {children}
+                <Analytics />
+            </main>
+
+            {/* Estos componentes solo se montarán en el cliente */}
+            <WhatsAppWidget
+                phoneNumber="+34647376782"
+                message="Hola! 👋🏼 ¿En qué podemos ayudarte?"
+                sendButton="Enviar"
+                companyName="Desatascos Pociten"
+                textReplyTime="En breve te responderemos. Muchas gracias"
+            />
+            <PhoneWidget />
+        </>
+    );
+}
+
+Layout.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default Layout;
