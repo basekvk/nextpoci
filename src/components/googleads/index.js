10535c397ee73f1c680293b2b9f1f3b5ad9544c6
David García <basek32@gmail.com>
2024-02-23 14:52:01 +0100

    update google ads tag

--- src/components/googleads/index.js
+++ src/components/googleads/index.js
@@ -1,22 +1,19 @@
 import Script from 'next/script';
 
 const GoogleAdsScript = () => {
-  return (
-    <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=AW-16469717403`}
-      />
-      <Script id="google-ads-script" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-16469717403');
-        `}
-      </Script>
-    </>
-  );
+    return (
+        <>
+            <script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=AW-16469717403"
+            ></script>
+            <script>
+                window.dataLayer = window.dataLayer || []; function gtag()
+                {dataLayer.push(arguments)}
+                gtag('js', new Date()); gtag('config', 'AW-16469717403');
+            </script>
+        </>
+    );
 };
 
 export default GoogleAdsScript;
