2df0ea4846cba9bf75298ac82f99127e8f8641cc
David García <basek32@gmail.com>
2024-03-18 19:44:44 +0100

    Update index.js

--- src/components/textFirst/index.js
+++ src/components/textFirst/index.js
@@ -15,7 +15,7 @@ export default function TextFirst() {
                         justifyContent: 'center',
                     }}
                 >
-                    Si estas aquí es porque seguramente necesites un servicio de desatascos, los atascos suelen ser más impredicibles de lo que nos pensamos, asi que vamos a intentar darte la solución que te mereces, localiza si tu problema esta presente en las siguientes imágenes y llamanos inmediatamente, antes de que te lo esperes, tu problema quedará totalmente resuelto con la máxima calidad y al mejor precio del mercado. ¡Compruébalo!
+                    Si estas aquí es porque seguramente necesites un servicio de desatascos, los atascos suelen ser más impredicibles de lo que nos pensamos, asi que vamos a intentar darte la solución que te mereces, localizamos tu problema y acabos con el, llámanos inmediatamente,y antes de que te lo esperes, tu problema quedará totalmente resuelto con la máxima calidad y al mejor precio del mercado. ¡Compruébalo!
                     
                    </p>
                 <br />
