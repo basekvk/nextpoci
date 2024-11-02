8fa3bcb1fa5986755579645fa582f4fbe9e81c7b
David García <basek32@gmail.com>
2024-11-02 12:49:24 +0100

    reset analitycs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,25 @@
-import React from 'react'; 
+import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import Script from 'next/script';
 import '../styles/tailwind-component.css';
 import Layout from '../components/layout/layout';
 import { ScrollToTop } from '../components/scroll';
 import GoogleAdsTag from '../components/googleads';
+import useGoogleAnalytics from '../hook/useGoogleAnalytics';
 import '../styles/globals.scss';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
+    useGoogleAnalytics(); // Execute the hook for Google Analytics
+
     return (
         <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                {/* Pre-connect to likely resources to speed up their loading time */}
                 <link rel="preconnect" href="https://www.google-analytics.com" />
                 <link rel="preconnect" href="https://www.googletagmanager.com" />
                 <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
@@ -24,6 +29,26 @@ function MyApp({ Component, pageProps }) {
                 <Component {...pageProps} />
                 <ScrollToTop />
                 <GoogleAdsTag />
+
+                {/* Google Tag Manager - Global site tag (gtag.js) */}
+                <Script
+                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                    strategy="afterInteractive"
+                />
+                <Script
+                    id="google-analytics-script"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                              page_path: window.location.pathname,
+                            });
+                        `,
+                    }}
+                />
             </Layout>
         </>
     );
