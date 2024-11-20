35cefe74d2dba73cdadffc6ba429b23e2e0acc23
David García <basek32@gmail.com>
2024-11-20 18:06:42 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -25,7 +25,32 @@ function MyApp({ Component, pageProps }) {
                 <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
             </Head>
 
+            {/* Google Tag Manager */}
+            <Script
+                id="google-tag-manager"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-NWFQKZVB');
+                    `,
+                }}
+            />
+
             <Layout>
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                    <iframe 
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-NWFQKZVB"
+                        height="0" 
+                        width="0" 
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    ></iframe>
+                </noscript>
+
                 <Component {...pageProps} />
                 <ScrollToTop />
                 <GoogleAdsTag />
